6f1802aacae74c8735765ab7d5a2b2fb797172f8
Westley Isbell <coolwest@gmail,com>
2020-10-29 16:21:02 -0500

    Remove unused code.  Trivia route moved to trivia controller in trivia module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,32 +1,13 @@
 import { Controller, Get, Render } from '@nestjs/common';
-import { AppService } from './app.service';
-import { TriviaGame } from './models/trivia-game';
-import * as data from './data/Apprentice_TandemFor400_Data.json';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
-
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-
   @Get()
   @Render('home')
   root() {
     return {
       title: 'Tandem Trivia',
       home_title_text: 'Welcome to Tandem Trivia!'
-    };
-  }
-
-  @Get('trivia')
-  @Render('trivia')
-  trivia() {
-    return {
-      title: 'Tandem Trivia',
-      game: new TriviaGame(10, data)
     };
   }
 }
