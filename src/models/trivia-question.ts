791e94f0d50632e77ae011f0d9321eb50c7384c1
Westley Isbell <coolwest@gmail,com>
2020-10-29 17:29:44 -0500

    Export shuffleArray function and import it into trivia-question model

--- src/models/trivia-question.ts
+++ src/models/trivia-question.ts
@@ -1,10 +1,11 @@
+import { shuffleArray } from "src/utility/utility";
 import { TriviaData } from "./trivia-data";
 
 /* Class resembling TriviaData but with more descriptive/specific property names */
 export class TriviaQuestion {
-  question: string;
-  incorrectAnswers: string[];
-  correctAnswer: string;
+  readonly question: string;
+  readonly incorrectAnswers: string[];
+  readonly correctAnswer: string;
 
   constructor(question: string, incorrectAnswers: string[], correctAnswer: string) {
     this.question = question;
@@ -20,5 +21,16 @@ export class TriviaQuestion {
         triviaData.correct
       );
     });
+  }
+
+  // Combines correctAnswer and incorrectAnswers into a single array
+  generateAnswerArray(): string[] {
+    return [...this.incorrectAnswers, this.correctAnswer];
+  }
+
+  // https://stackoverflow.com/a/12646864
+  // Gets incorrect and correct answers and returns them in a random order
+  randomizeAnswers() {
+    return shuffleArray(this.generateAnswerArray());
   }
 }
