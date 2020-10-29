616d3539475725dab7db425c1b90f52729fd3876
Westley Isbell <coolwest@gmail,com>
2020-10-28 23:59:22 -0500

    Install bulma and create basic navbar and homepage

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('home')
+  root() {
+    return {
+      title: 'Tandem Trivia',
+      home_title_text: 'Welcome to Tandem Trivia!'
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost, AbstractHttpAdapter } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
+import { Express } from "express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  /* Add necessary node modules for use in front end without full node_modules file access */
+  const adapterHost: HttpAdapterHost = app.get(HttpAdapterHost);
+  const httpAdapter: AbstractHttpAdapter = adapterHost.httpAdapter;
+  const expressInstance: Express = httpAdapter.getInstance();
+
+  expressInstance.get('/css/bulma.min.css', function (req, res) {
+    res.sendFile(join(__dirname, '..', 'node_modules/bulma/css/bulma.min.css'));
+  });
+  /* END: Add necessary node modules */
+
+  /* Add all view directories */
+  app.setBaseViewsDir([
+    join(__dirname, '..', 'src', 'views', 'home'),
+  ]);
+
+  app.setViewEngine('pug');
+
   await app.listen(3000);
 }
 bootstrap();
