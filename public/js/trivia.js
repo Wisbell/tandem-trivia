7f8d799165aa71672642387c03ab80cc24de8a2d
Westley Isbell <coolwest@gmail,com>
2020-10-29 17:30:35 -0500

    WIP: Add necessary buttons for future features.  Add ability to hide starting section when ready to begin

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -1,13 +1,28 @@
-console.log("Trivia loaded!");
-
 class TriviaGame {
-  hasStarted = false;
-  currentQuestion;
-  questions;
+  // hasStarted = false;
+  // currentQuestion;
+  // questions;
 
   constructor() {
 
   }
+
+  // Hide start section
+  // Display question section
+  start() {
+    console.log('start clicked!');
+    const goodLuckSection = document.getElementById('startSection');
+    const triviaSection = document.getElementById('triviaSection');
+    goodLuckSection.classList.add('is-hidden');
+    triviaSection.classList.remove('is-hidden');
+  }
+
+  // Hide current question
+  // Show next question if not at the end of the question list
+  nextQuestions() {
+
+  }
 }
 
+// Global class keeps track of game state
 const Game = new TriviaGame();
