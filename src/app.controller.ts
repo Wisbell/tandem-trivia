8acb56f443c3f590efb06340166e1a6bff4b722f
Westley Isbell <coolwest@gmail,com>
2020-10-29 12:44:55 -0500

    Add passing of trivia data to trivia view

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TriviaGame } from './models/trivia-game';
+import * as data from './data/Apprentice_TandemFor400_Data.json';
 
 @Controller()
 export class AppController {
@@ -24,7 +26,7 @@ export class AppController {
   trivia() {
     return {
       title: 'Tandem Trivia',
-      home_title_text: 'Welcome to Tandem Trivia!'
+      game: new TriviaGame(10, data)
     };
   }
 }
