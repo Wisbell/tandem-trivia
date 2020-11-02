e4bae252906ad8f61f59b3978146bae639d8f406
Westley Isbell <coolwest@gmail,com>
2020-11-01 18:08:09 -0600

    Add test to make sure home route is rendered

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
+import { join } from 'path';
+import * as pug from 'pug';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestExpressApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestExpressApplication>();
+    app.setBaseViewsDir([
+      join(__dirname, '..', 'src', 'views', 'home'),
+      join(__dirname, '..', 'src', 'views', 'trivia'),
+      join(__dirname, '..', 'src', 'views', 'about')
+    ]);
+    app.setViewEngine('pug');
     await app.init();
   });
 
   it('/ (GET)', () => {
+    const pugRenderedHtml = pug.renderFile(
+      join(__dirname, '..', 'src', 'views', 'home', 'home.pug'),
+      {
+        title: 'Tandem Trivia',
+        home_title_text: 'Welcome to Tandem Trivia!'
+      }
+    );
+
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect(pugRenderedHtml);
   });
 });
